77b6965d92ca8079ea264bc198377b3571a7479f
Cascades <5692370+Cascades@users.noreply.github.com>
2021-04-15 18:22:18 +0100

    initial commit

---
+++ many_lights/many_lights.cpp
@@ -0,0 +1,12 @@
+﻿// many_lights.cpp : Defines the entry point for the application.
+//
+
+#include "many_lights.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
